0a8df65c006e42d5f736426f3ae4ad4ec2c2febc
Miguelmo <miguelmoyaortega@gmail.com>
2017-11-19 20:48:47 +0100

    calendario

--- src/app/calendario/calendario/calendario.component.ts
+++ src/app/calendario/calendario/calendario.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+declare let $: any;
 
 @Component({
-  selector: 'app-calendario',
-  templateUrl: './calendario.component.html',
-  styleUrls: ['./calendario.component.css']
+    selector: 'app-calendario',
+    templateUrl: './calendario.component.html',
+    styleUrls: ['./calendario.component.css']
 })
-export class CalendarioComponent implements OnInit {
+export class CalendarioComponent implements OnInit, AfterViewInit {
+    fullCalendar;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.fullCalendar = $('#fullCalendar');
+    }
 
+    ngAfterViewInit() {
+        this.fullCalendar.fullCalendar({
+            timeFormat: 'H:mm',
+            locale: 'es'
+        });
+    }
 }
