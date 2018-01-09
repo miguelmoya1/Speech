f294e3ddd7c40d332111685a852302c0f72c94ae
Miguelmo <miguelmoyaortega@gmail.com>
2018-01-09 20:02:03 +0100

    iconos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -70,15 +70,15 @@ export class MenuComponent implements OnInit {
     cantRegister(): boolean | string {
         if (this.userRegister.name && this.userRegister.name.trim() !== '')
             if (this.userRegister.surname && this.userRegister.surname.trim() !== '')
-                if (this.userRegister.email && this.emailExp.test(this.userRegister.email))
-                    if (this.userRegister.password && this.userRegister.password.length >= 6)
-                        if (this.password2 && this.password2 === this.userRegister.password)
-                            if (this.userRegister.nick && this.userRegister.nick.trim() !== '')
-                                return false;
-                            else return 'El nick es necesario'; // TODO: Comprobar en la base de datos que no exista
-                        else return 'Las contraseñas tienen que coincidir';
-                    else return 'La contrasña tiene que ser de 6 caracteres mínimos';
-                else return 'El correo no es válido';
+                // if (this.userRegister.email && this.emailExp.test(this.userRegister.email))
+                if (this.userRegister.password && this.userRegister.password.length >= 6)
+                    if (this.password2 && this.password2 === this.userRegister.password)
+                        if (this.userRegister.nick && this.userRegister.nick.trim() !== '')
+                            return false;
+                        else return 'El nick es necesario'; // TODO: Comprobar en la base de datos que no exista
+                    else return 'Las contraseñas tienen que coincidir';
+                else return 'La contrasña tiene que ser de 6 caracteres mínimos';
+            // else return 'El correo no es válido';
             else return 'El apellido es necesario';
         else return 'El nombre es necesario';
     }
